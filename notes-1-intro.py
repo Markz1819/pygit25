bdc14d6104121d7f5b0f88506e0016012cb4fa38
Markz1819 <marktinyuen@gmail.com>
2025-10-07 10:09:32 -0700

    yup

---
+++ notes-1-intro.py
@@ -0,0 +1,46 @@
+# Notes - Introduction
+# 16 September
+# Mark Zhang
+
+# Create an algorithm to solve a problem
+# Problem: Create our own chatbot
+# MeGPT
+
+#1. Greet the user with a predetermined statement
+greeting = "Hello! I am a chatbot"
+print(greeting)
+
+print("hi im MeGPT")
+print("unlike the other guy")
+print("I'm completely deterministic")
+
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}!")
+
+
+age = input("How old are you? ")
+print(f"You are {age} years old.")
+
+color = input("What is your favourite color? ")
+print(f"Your favourite color is {color}.")
+
+food = input("What is your favourite food? ")
+print(f"Your favourite food is {food}.")
+
+user_name = "Mark"
+
+# 8. See IF the user is someone specific.
+# 8a. If they're someone specific, tell them a secret
+if user_name == "Mark":
+    print("I like mac and cheese")
+
+user_name = "Mark"
+
+# 8. See IF the user is someone specific.
+# 8a. If they're someone specific, tell them a secret
+if user_name == "Mark":
+    print("I like mac and cheese")
+
+    favourite_book = input("What is your favourite book? ")
+    if favourite_book == "Harry Potter":
+        print("I love Harry Potter too!")
